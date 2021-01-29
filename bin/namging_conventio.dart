4d71beb8e3b9037e309de5caaae200f72846a1f4
Sayed-Noman <Sayed.siddik@northsouth.edu>
2021-01-29 23:10:30 +0600

    naming convention added

---
+++ bin/namging_conventio.dart
@@ -0,0 +1,8 @@
+/// Identifire Name: UpperCamaleCase
+///Library and File Name: lower_case_with_underscore
+///import package: lower_case_with_underscore
+///Constant name : lowerCamelCase
+///Other identifire name: lowerCamaelCase
+
+/// FOR MORE INFORMATION VISIT:-
+/* https://dart.dev/guides  */
